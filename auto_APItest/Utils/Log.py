1f172a630165ea5b3602ab9d63b88172e362faf3
lianhuabai <1059161810@qq.com>
2020-04-14 14:05:10 +0800

    'update'

--- auto_APItest/Utils/Log.py
+++ auto_APItest/Utils/Log.py
@@ -14,11 +14,12 @@ LEVELS = {
     'error':logging.ERROR,
     'critical':logging.CRITICAL
 }
-
+#初始化日志对象logger
 logger = logging.getLogger()
 level = 'default'
 
 def create_file(file):
+    #路劲从右查询第一个/位置切片，获取log所在文件的路劲
     path = file[0:file.rfind('/')]
     if not os.path.isdir(path):
         os.makedirs(path)
@@ -27,12 +28,13 @@ def create_file(file):
         fd.close()
     else:
         pass
-
-def set_handler(levels):
+#为logger添加写入对应的handler
+def add_handler(levels):
     if levels == 'error':
         logger.addHandler(MyLog.err_handler)
     logger.addHandler(MyLog.handler)
 
+#调用log记录日志后移除logger中的handler，防止出现重复日志信息
 def remove_handler(levels):
     if levels == 'error':
         logger.removeHandler(MyLog.err_handler)
@@ -45,43 +47,53 @@ def get_current_time():
 
 
 class MyLog:
-    path = os.path.dirname(os.path.dirname(os.path(__file__)))
+    #获取当前脚本文件父类的绝对路径（项目主目录）
+    path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     log_file = path+'/Logs/log.log'
     err_file = path+'/Logs/err.log'
+    #设置日志等级
     logger.setLevel(LEVELS.get(level,logging.NOTSET))
+    #创建日志文件
     create_file(log_file)
     create_file(err_file)
     date = '%Y-%m-%d %H-%M-%S'
-
+    #实例化日志信息输出到磁盘文件上的流，指定文件路劲和编码
     handler = logging.FileHandler(log_file,encoding='utf-8')
     err_handler = logging.FileHandler(err_file,encoding='utf-8')
 
     @staticmethod
     def debug(log_msg):
-        set_handler('debug')
+        add_handler('debug')
         logger.debug("[DEBUG" + get_current_time() + "]" + log_msg)
         remove_handler('debug')
 
     @staticmethod
-    def debug(log_msg):
-        set_handler('warning')
-        logger.debug("[WARNING" + get_current_time() + "]" + log_msg)
+    def warning(log_msg):
+        add_handler('warning')
+        logger.warning("[WARNING" + get_current_time() + "]" + log_msg)
         remove_handler('WARNING')
 
     @staticmethod
-    def debug(log_msg):
-        set_handler('error')
-        logger.debug("[ERROR" + get_current_time() + "]" + log_msg)
+    def error(log_msg):
+        add_handler('error')
+        logger.error("[ERROR" + get_current_time() + "]" + log_msg)
         remove_handler('error')
 
     @staticmethod
-    def debug(log_msg):
-        set_handler('info')
-        logger.debug("[INFOR" + get_current_time() + "]" + log_msg)
+    def info(log_msg):
+        add_handler('info')
+        logger.info("[INFO" + get_current_time() + "]" + log_msg)
         remove_handler('info')
 
     @staticmethod
-    def debug(log_msg):
-        set_handler('critical')
-        logger.debug("[CRITICAL" + get_current_time() + "]" + log_msg)
+    def critical(log_msg):
+        add_handler('critical')
+        logger.critical("[CRITICAL" + get_current_time() + "]" + log_msg)
         remove_handler('critical')
+
+if __name__ == '__main__':
+    MyLog.debug("This is debug message")
+    MyLog.info("This is info message")
+    MyLog.error("This is error msessage")
+    MyLog.warning("This is warning message")
+    MyLog.critical("This is critical message")
--- auto_APItest/readme.md
+++ auto_APItest/readme.md
@@ -1,6 +1,9 @@
 ## 接口自动化测试文档  
 
-### 基于python的requests,使用Pytest框架实现接口自动化  
+### 环境准备  
+* 语言 python3  
+* 库 pytest、requests、allure-pytest (pytest-allure-adaptor已停止更新，python3开始改用allure-pytest)  
+* 开源测试报告框架 allure2(需要Java环境，jdk1.8+)
 
 ### 项目目录结构  
 
@@ -18,13 +21,51 @@
 * Logs 日志文件
 * run.py 主程序入口，执行测试  
 ---
-####markdown常用语法
-'#' 标题  
-'*' 列表
-'---' 横线  
-反引号包裹为代码  
-三反引号包裹为代码块  
-星号包裹为加粗  
-双引号包裹为斜体
-----
+### 代码解析  
+*配置文件封装 ./Config/config.py*  
+* 封装定义ConfigParser  
+```
+#实例化类ConfigParser
+    config = ConfigParser()
+#配置文件读写方法
+    def get_config(self,title,value):
+        return self.config.get(title,value)
+
+    def set_config(self,title,value,text):
+        self.config.set(title,value,text)
+        with open(self.congfig_path,'wb') as f:
+            return self.config.write(f)
+
+    def add_config(self,title):
+        self.config.add_section(title)
+        with open(self.config_path,'wb') as f:
+            return self.config.write(f)
+```
+*日志封装./Utils/Log.py*  
+* 创建日志文件函数  
+```
+#判断传入的路劲/文件是否存在
+def create_file(file):
+    path = file[0:file.rfind('/')]
+    if not os.path.isdir(path):
+        os.makedirs(path)
+    if not os.path.isfile(file):
+        fd = open(file,mode='w',encoding='utf-8')
+        fd.close()
+    else:
+        pass
+```
+* 实例化日志写入流FileHandler,指定文件路劲和编码方式
+```
+handler = logging.FileHandler(log_file,encoding='utf-8')
+```
+* 日志写入函数  
+```
+#add Handler后再次调用此时logger存在多个Handler导致日志重复，需要后置处理remove掉Handler
+    def warning(log_msg):
+        add_handler('warning')
+        logger.warning("[WARNING" + get_current_time() + "]" + log_msg)
+        remove_handler('WARNING')
+        
+```
 
