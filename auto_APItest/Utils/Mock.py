bfc6d65945788c3bea6db229acd7fdcc3d8f1b07
lianhuabai <1059161810@qq.com>
2020-07-25 00:12:55 +0800

    mockserver

--- auto_APItest/Utils/Mock.py
+++ auto_APItest/Utils/Mock.py
@@ -3,4 +3,24 @@
 # @Author : tester_ye 
 # @File : Mock.py
 
-from unittest import mock
+from flask import Flask,jsonify,request
+
+mock_server = Flask(__name__)
+mock_server.config['JSON_AS_ASCII'] = False
+
+valuation = {
+    "message":"OK",
+    "code":0,
+    "result":{
+        "to_asset":"ZTB",
+        "amount":"68.26819141",
+        "price":"1.36536382哈哈哈",
+        "limit_status":0}
+}
+@mock_server.route("/valuation",methods=['GET'])
+def get_valuation():
+    return jsonify(valuation)
+
+if __name__ == '__main__':
+    mock_server.run(host='127.0.0.1',port=7777,debug=True)
+
